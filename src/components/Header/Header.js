6d9c18e52511d257c2e3914f34595ba9872ed789
David Salazar <34181460+mrgold92@users.noreply.github.com>
2021-03-25 17:52:51 +0100

    Implantado modo oscuro

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,23 @@
 import "./header.css";
 
 const Header = () => {
+  const changeCheck = () => {
+    const check = document.getElementById("checkbox");
+
+    console.log(check.checked);
+
+    if (check.checked === true) {
+      document.body.classList.toggle("dark-theme");
+    }else{
+     document.body.classList.remove("dark-theme")
+    }
+  };
   return (
-    <div className="menu">
-      <div className="color">
-        <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round"></span>
+    <div className='menu'>
+      <div className='color'>
+        <label className='switch'>
+          <input type='checkbox' id='checkbox' onClick={() => changeCheck()} />
+          <span className='slider round'></span>
         </label>
       </div>
     </div>
